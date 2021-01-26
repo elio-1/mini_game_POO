8749a161714e0e9a66bfb1a5a08756841106a025
elio75006 <elioraph38@gmail.com>
2021-01-26 15:44:08 +0100

    features:basics mecanics

--- app.rb
+++ app.rb
@@ -3,5 +3,26 @@ Bundler.require
 
 require_relative 'lib/game'
 require_relative 'lib/player'
-
 binding.pry
+player1 = Player.new("José")
+player2 = Player.new("Josiane")
+puts "Current players states: "
+puts player1.player_state
+p player2.player_state
+puts "FIGHT !"
+while player1.health > 0 && player2.health > 0 do
+  if player2.health <= 0 || player1.health <= 0 
+    break
+  else
+  puts "Attack phase:"
+  player2.attacks(player1)
+  player1.attacks(player2)
+  puts
+  puts "Player state:"
+  puts
+  puts player1.player_state
+  puts player2.player_state
+  puts "------------"
+  end
+end
+
--- lib/player.rb
+++ lib/player.rb
@@ -6,25 +6,34 @@ class Player                         # create player class
     @health = 10                     # Set basic life points to 10
     @@players << self                # add new player to class variable @@players
   end
-  def player_state                   # players current health state 
-      @@players.each do |player|  
-      puts "#{player.name} has #{player.health} health points left."
-      end
-  end
-  def get_damage(damage)
-    @health -= damage                 # soustract current player heatlh by the damage he takes
+  
+  def player_state                   # player current health state 
     if @health <= 0
       puts "#{@name} is dead !"
+    else
+      puts "#{@name} has #{@health} health points left."
     end
-    player_state                      # player current health state (l9)
+  end
+  
+  def show_state                      # all players current health state 
+    @@players.each do |player|  
+      player.player_state
+    end
+  end
+  
+  def get_damage(damage)
+    @health -= damage                 # soustract current player heatlh by the damage he takes    
+    puts ">#{self.name} inflict #{damage} damage(s).<"
   end
   
   def attacks(player)                 # attack a player
-    puts "#{self.name} attacks #{player.name} !"
     player.get_damage(compute_damage) # compute damage taken by the player
+    puts "#{self.name} attacks #{player.name} !"
+    
+    
   end
   
   def compute_damage
     return rand(1..6)
   end
-end # end of the player class
+end                                   # end of the player class
